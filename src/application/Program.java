0040aa1b97e14891b38f638605c62170106a66cc
AlexandreSteinbach <alexandresteinbach2@gmail.com>
2026-02-11 20:57:32 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,13 @@
 package application;
 
+import application.boardgame.Position;
+
 public class Program {
     public static void main() {
+
+        Position pos = new Position(3, 5);
+
+        System.out.print(pos);
 
     }
 }
