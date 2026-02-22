5500f0faf30a84e57d1c3483ff8e0b4ac7d9fa09
AlexandreSteinbach <alexandresteinbach2@gmail.com>
2026-02-22 18:32:41 -0300

    Chess layer and printing board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,13 @@
 package application;
 
-import application.boardgame.Position;
+import application.boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
     public static void main() {
 
-        Position pos = new Position(3, 5);
-
-        System.out.print(pos);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
 
     }
 }
