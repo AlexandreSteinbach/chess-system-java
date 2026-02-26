b2667f0743b84ff2c3e035d8ce6096d46742ae94
AlexandreSteinbach <alexandresteinbach2@gmail.com>
2026-02-25 21:23:35 -0300

    Knight

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -215,11 +215,13 @@ public class ChessMatch {
         placeNewPiece('h' , 1, new King(board, Color.WHITE));
         placeNewPiece('e', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('e', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('d', 4, new Knight(board, Color.WHITE));
 
         placeNewPiece('a', 8, new King(board, Color.BLACK));
         placeNewPiece('b', 8, new Rook(board, Color.BLACK));
         placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('e', 5, new Knight(board, Color.BLACK));
 
         }
 }
